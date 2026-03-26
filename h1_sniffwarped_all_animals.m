dd211d5c0fabee9408aa850aa2ca9448972a7e26
Andrea K Barreiro <abarreiro@smu.edu>
2026-03-26 13:01:58 -0500

    Update h1_sniffwarped_all_animals.m
    Per animal assessment of h1, use sniff-warped phase

--- h1_sniffwarped_all_animals.m
+++ h1_sniffwarped_all_animals.m
@@ -1,5 +1,6 @@
-%set_paths_AKB
-dirloc = '/Users/sharanaparvin/Fall 25 Research/Respiratory phase selectivity/';
+set_paths_AKB
+%dirloc = '/Users/sharanaparvin/Fall 25 Research/Respiratory phase selectivity/';
+
 load(fullfile(dirloc,'phase_selectivity_survey.mat'));
 
 nAnimals = length(Survey);
@@ -8,6 +9,16 @@ percentRetro_per_animal = nan(nAnimals,1);
 nCoupled_per_animal     = zeros(nAnimals,1);
 nRetro_per_animal       = zeros(nAnimals,1);
 nMissing_per_animal     = zeros(nAnimals,1);
+
+% What is the "null hypothesis" for cells are more likely 
+% to be selective during retronasal phase (i.e. is it possible the
+% preferred phases are uniformly distributed thoroughout breath cycle?)
+%
+nullSignificantPhasePerc = zeros(nAnimals,1);
+
+% For "phase-coupled" cells, get preferred phase
+phiCoupled = cell(nAnimals,1);
+ItoEConst = zeros(nAnimals,1);
 
 for animalID = 1:nAnimals
 
@@ -48,6 +59,10 @@ for animalID = 1:nAnimals
         end
     end
 
+    % Constant for the animal, only need to evaluate once
+    ItoEConst(animalID) = entry.ItoE;
+    nullSignificantPhasePerc(animalID) = 1- (entry.ItoE)/(2*pi);
+
     % sniff-warped retro definition
     isRetroPref = phi_pref_array > ItoE_array;
 
@@ -61,6 +76,8 @@ for animalID = 1:nAnimals
     nCoupled_per_animal(animalID) = nCoupledOverall;
     nRetro_per_animal(animalID)   = nRetroOverall;
 
+    phiCoupled{animalID} = phi_pref_array(validCoupled);
+    
     if nCoupledOverall > 0
         percentRetro_per_animal(animalID) = nRetroOverall / nCoupledOverall;
     end
@@ -105,29 +122,39 @@ title('Hypothesis 1 across animals (sniff-warped)');
 ylim([0 100]);
 grid on;
 hold off;
+
+
+figure;
+for j1=1:nAnimals
+    subplot(2,4,j1);
 %polar plot
 % preferred phases of only coupled cells
-% phi_coupled = phi_pref_array(coupled_array);
-% 
-% % remove NaN just in case
-% phi_coupled = phi_coupled(~isnan(phi_coupled));
-% 
-% figure('Color','w');
-% polarhistogram(phi_coupled, 24, ...
-%     'FaceColor', [0.2 0.5 0.85], ...
-%     'FaceAlpha', 0.8);
-% title('Preferred phases of coupled cells');
-% 
-% hold on;
-% 
-% % mark retro range boundaries: pi and 2*pi
-% rl = rlim;
-% polarplot([pi pi], [0 rl(2)], 'r--', 'LineWidth', 2);
-% 
-% % 2*pi is same as 0, so draw at angle 0
-% polarplot([0 0], [0 rl(2)], 'r--', 'LineWidth', 2);
-% 
-% hold off;
+    if (0)
+    phi_coupled = phi_pref_array(coupled_array);
+    % remove NaN just in case
+    phi_coupled = phi_coupled(~isnan(phi_coupled));
+    else
+        phi_coupled = phiCoupled{j1};
+    end
+    %figure('Color','w');
+    polarhistogram(phi_coupled, 24, ...
+        'FaceColor', [0.2 0.5 0.85], ...
+        'FaceAlpha', 0.8);
+    title(sprintf('Animal %d',j1));
+    
+    hold on;
+    
+    % mark retro range boundaries: pi and 2*pi
+    rl = rlim;
+    polarplot([ItoEConst(j1) ItoEConst(j1)], [0 rl(2)], 'r--', 'LineWidth', 2);
+    
+    % 2*pi is same as 0, so draw at angle 0
+    polarplot([0 0], [0 rl(2)], 'r--', 'LineWidth', 2);
+    
+    hold off;
+end
+
+
 phi_coupled = phi_pref_array(coupled_array);
 phi_coupled = phi_coupled(~isnan(phi_coupled));
 
